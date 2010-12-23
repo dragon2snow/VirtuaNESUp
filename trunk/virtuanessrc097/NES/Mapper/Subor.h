d37ceff98ca5a1364a32d2d63bc68d89b6bcb72e
tensai.wang@gmail.com <tensai.wang@gmail.com@3ce6dcce-da53-11de-ad86-bdb49d26ad51>
2010-12-23 07:15:47 +0000

    增加mapper168支持新dump的小霸王999学习卡 增加BOARD SUBOR_999(对应mapper168)

---
+++ trunk/virtuanessrc097/NES/Mapper/Subor.h
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////
+// Subor                                                                //
+//////////////////////////////////////////////////////////////////////////
+class	MapperSubor999 : public Mapper
+{
+public:
+	MapperSubor999( NES* parent ) : Mapper(parent) {}
+
+	void	Reset();
+	//void	Read( WORD addr, BYTE data );
+	//BYTE	ReadLow( WORD addr, BYTE data );
+	void	WriteLow( WORD addr, BYTE data );
+	//void	Write( WORD addr, BYTE data );
+
+	// For state save
+	BOOL	IsStateSave() { return TRUE; }
+	void	SaveState( LPBYTE p );
+	void	LoadState( LPBYTE p );
+
+protected:
+	BYTE	reg5200,reg5000;
+	void	SetBank_CPU(void);
+private:
+};
