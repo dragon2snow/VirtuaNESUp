57085b9660b450832773baf69082807d4f960afc
tensai.wang@gmail.com <tensai.wang@gmail.com@3ce6dcce-da53-11de-ad86-bdb49d26ad51>
2010-12-28 07:53:02 +0000

    改进mapper168(SUBOR_999)的代码

--- trunk/virtuanessrc097/NES/Mapper/Subor.cpp
+++ trunk/virtuanessrc097/NES/Mapper/Subor.cpp
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////////
 void	MapperSubor999::Reset()
 {
-	reg5200 = reg5000 = 0;
+	reg5200 = reg5000 = reg480d = 0;
 	SetBank_CPU();
 }
 
@@ -15,13 +15,22 @@ void	MapperSubor999::WriteLow( WORD addr, BYTE data )
 	}else if(addr==0x5200){
 		reg5200 = data&7;
 		SetBank_CPU();
+	}else if(addr==0x480d){
+		reg480d = data;
+	}
+	
+	// $6000-$7FFF WRAM
+	if( addr >= 0x6000 && addr <= 0x7FFF ) {
+		CPU_MEM_BANK[addr>>13][addr&0x1FFF] = data;
 	}
 }
 
 	
 void MapperSubor999::SetBank_CPU(void)
 {	
-	if(reg5200<7){
+	//SetPROM_8K_Bank(3,0);
+
+	if(reg5200<4){
 		SetPROM_16K_Bank(4,reg5000);
 		SetPROM_16K_Bank(6,0);
 	}else{
